4608f84b20276a67d10401eb8f273860819e0c82
Nathan Garrow <nathan@rftb.agency>
2022-08-01 16:06:52 -0500

    setup: connected to Postgres DB

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '6d1395db868945ae20c9e8e25ce060fb'),
+    secret: env("ADMIN_JWT_SECRET", "6d1395db868945ae20c9e8e25ce060fb"),
+    apiToken: { salt: env("API_TOKEN_SALT") },
   },
 });
