3d8481a66ea5d7317a68a97c651b6e6914e24ad8
Nathan Garrow <nathan@rftb.agency>
2022-08-02 08:50:49 -0500

    setup: added performer, speaker, and event collection types

---
+++ src/api/performer/controllers/performer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  performer controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::performer.performer');
---
+++ src/api/performer/routes/performer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * performer router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::performer.performer');
